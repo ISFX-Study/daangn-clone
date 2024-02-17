bc15a204b4edfcdadd28aab0161db62fda7c73af
pej4303 <pej4303@gmail.com>
2024-02-18 06:30:59 +0900

    프로젝트 셋팅

---
+++ src/main/java/com/clone/daangn/DaangnApplication.java
@@ -0,0 +1,12 @@
+package com.clone.daangn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaangnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaangnApplication.class, args);
+	}
+}
---
+++ src/test/java/com/clone/daangn/DaangnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clone.daangn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaangnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
