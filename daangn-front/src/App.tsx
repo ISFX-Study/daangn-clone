1a54f18490a09fac063786a784fef9119019ea02
pej4303 <pej4303@gmail.com>
2024-03-16 23:28:14 +0900

    메인 화면 구성

--- daangn-front/src/App.tsx
+++ daangn-front/src/App.tsx
@@ -3,24 +3,26 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import Layout from './components/layout';
+
 function App() {
     const [count, setCount] = useState(0)
     const [message, setMessage] = useState("");
 
     const url = '/api/test/hello'; 
 
-    useEffect(() => {
-        // fetch에 대해서 알아볼것
-        fetch(url)
-            .then(response => response.text())
-            .then(message => {
-                setMessage(message);
-        });    
-    },[])
+    // useEffect(() => {
+    //     // fetch에 대해서 알아볼것
+    //     fetch(url)
+    //         .then(response => response.text())
+    //         .then(message => {
+    //             setMessage(message);
+    //     });    
+    // },[])
 
     return (
         <>
-        <div>
+        {/* <div>
             <a href="https://vitejs.dev" target="_blank">
             <img src={viteLogo} className="logo" alt="Vite logo" />
             </a>
@@ -39,7 +41,19 @@ function App() {
         </div>
         <p className="read-the-docs">
             Click on the Vite and React logos to learn more
-        </p>
+        </p> */
+        <Layout />
+        }
+        {/* <nav>
+            <ul>
+                <li><a href="#">당근</a></li>
+                <li><a href="#">중고거래</a></li>
+                <li><a href="#">동네업체</a></li>
+                <li><a href="#">알바</a></li>
+                <li><a href="#">부동산 직거래</a></li>
+                <li><a href="#">중고차 직거래</a></li>
+            </ul>
+        </nav> */}
         </>
     )
 }
