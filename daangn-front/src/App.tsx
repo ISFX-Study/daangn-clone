33ea1ce68108a0ccd927d6d3009e438454973455
pej4303 <pej4303@gmail.com>
2024-03-18 23:16:30 +0900

    css import test

--- daangn-front/src/App.tsx
+++ daangn-front/src/App.tsx
@@ -1,6 +1,7 @@
 /* react-ruuter 불러오기 */
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import './App.css'
+import './App.css';
+// 왜 오류지..??
 // import './src/styles/componet.css'
 
 /* 페이지 불러오기 */
@@ -10,20 +11,20 @@ import CompanyList from './pages/companyList';  // 동네업체
 import PartTimeList from './pages/partTimeList';  // 알바
 import HouseList from './pages/houseList';  // 부동산직거래
 import CarList from './pages/carList';  // 중고차직거래
-import Sample from './pages/sample'
+import Sample from './pages/sample';
 
 function App() {
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path="/productList" element={<ProductList />} />
-            <Route path="/companyList" element={<CompanyList />} />
-            <Route path="/partTimeList" element={<PartTimeList />} />
-            <Route path="/houseList" element={<HouseList />} />
-            <Route path="/carList" element={<CarList />} />
-            <Route path="/sample" element={<Sample />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/productList" element={<ProductList />} />
+        <Route path="/companyList" element={<CompanyList />} />
+        <Route path="/partTimeList" element={<PartTimeList />} />
+        <Route path="/houseList" element={<HouseList />} />
+        <Route path="/carList" element={<CarList />} />
+        <Route path="/sample" element={<Sample />} />
+      </Routes>
     </BrowserRouter>
   );
 }
